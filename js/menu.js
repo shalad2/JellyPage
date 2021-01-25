f5dfc687023f169009d8201504f9604a70486fc5
sharada <harada.shogo@d.mbox.nagoya-u.ac.jp>
2021-01-25 15:09:20 +0900

    air

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,37 @@ $(function() {
         }
     });
 });
+
+function loadMenu(rootDir){
+    var html = "";
+    html += '<aside class="side-bar">';
+    html += '<h1><a href="' + rootDir + 'index.html"><img class="menu-logo" src="' + rootDir + 'images/logo.png" alt="JellyPageホーム"></a></h1>';
+    html += '<nav>' +
+                '<ul class="menu-click">' +
+                    '<li>' +
+                        '<div class="menu">GROMACS</div>' +
+                        '<ul>' +
+                            '<li><a href="' + rootDir + 'gromacs/gromacs_tutorial1_top.html">GROMACSチュートリアル①</a></li>' +
+                            '<li>CLICKメニュー2</li>' +
+                            '<li>CLICKメニュー3</li>' +
+                            '<li>CLICKメニュー4</li>' + 
+                            '<li>CLICKメニュー5</li>' +
+                        '</ul>' +
+                    '</li>' +
+                '</ul>' +
+                '<ul class="menu-click">' +
+                    '<li>' +
+                        '<div class="menu">LAMMPS</div>' +
+                        '<ul>' +
+                            '<li>メニュー1</li>' +
+                            '<li>メニュー2</li>' +
+                            '<li>メニュー3</li>' +
+                            '<li>メニュー4</li>' + 
+                            '<li>メニュー5</li>' +
+                        '</ul>' +
+                    '</li>' +
+                '</ul>' +
+            '</nav>';
+    html += '</aside>';
+    document.write(html);
+}
